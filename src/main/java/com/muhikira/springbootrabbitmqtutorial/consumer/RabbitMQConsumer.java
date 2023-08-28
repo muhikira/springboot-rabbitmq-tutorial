de054fa152e6c90db0185b61426735dda402876c
Muhikira <muhikiraismael@gmail.com>
2023-08-28 09:53:23 -0500

    RabbitMQ first commit

--- src/main/java/com/muhikira/springbootrabbitmqtutorial/consumer/RabbitMQConsumer.java
+++ src/main/java/com/muhikira/springbootrabbitmqtutorial/consumer/RabbitMQConsumer.java
@@ -6,8 +6,8 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RabbitMQC {
-    private static final Logger LOGGER = LoggerFactory.getLogger (RabbitMQC.class);
+public class RabbitMQConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger (RabbitMQConsumer.class);
 
     @RabbitListener(queues = {"${rabbitmq.queue.name}"})
     public void consume(String message) {
